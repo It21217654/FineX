a102c0dd2a38ad1e55a433d3f0842a710ed88c7a
Your GitHub username <it21217654@my.sliit.lk>
2023-05-14 21:55:26 +0530

    "Last commit"

--- app/src/main/java/com/example/madp/DashboardActivity.kt
+++ app/src/main/java/com/example/madp/DashboardActivity.kt
@@ -13,18 +13,24 @@ class DashboardActivity : AppCompatActivity() {
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Set click listeners for the buttons in the layout
+
+        // Button to navigate to LoginActivity
         binding.button9.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
 
+        // Button to navigate to Post1Activity
         binding.button7.setOnClickListener {
             startActivity(Intent(this, Post1Activity::class.java))
         }
 
+        // Button to navigate to Payment1Activity
         binding.button8.setOnClickListener {
             startActivity(Intent(this, Payment1Activity::class.java))
         }
 
+        // Button to navigate to ApplyJob1Activity
         binding.button.setOnClickListener {
             startActivity(Intent(this, ApplyJob1Activity::class.java))
         }
